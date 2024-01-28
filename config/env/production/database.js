134f633c35beb15e07608b3d127f46448764c703
NordWebDuo <contact@nordwebduo.com>
2024-01-28 19:02:20 +0000

    modified database

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
       connection: {
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        database: env('DATABASE_NAME', 'andrei11_strapi'),
+        user: env('DATABASE_USERNAME', 'andrei11_root'),
+        password: env('DATABASE_PASSWORD', 'q1q2w1w2A!'),
         schema: env('DATABASE_SCHEMA', 'public'), // Not required
         ssl: {
           ca: env('DATABASE_CA')
